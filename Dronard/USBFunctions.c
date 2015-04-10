2af49ca0aa0b38534d05c9bea159d84baa2826c7
DmasterD <meijer420@hotmail.com>
2015-04-10 11:21:15 +0200

    USBFunctions.c

---
+++ Dronard/USBFunctions.c
@@ -0,0 +1,45 @@
+#include "USBFunctions.h"
+
+int OpenAPM(void){
+	int fd;
+	fd = serialOpen(APMDEVICE, BAUDRATE);
+	
+	if(fd == -1) {
+		printf("Couldn't open APM device.\nError: %d", fd);
+		exit(fd);
+	}
+	
+	printf("Succesfully opened APM device.");
+
+	return fd;
+};
+
+void CloseAPM(int fd){
+	serialClose(fd);
+};
+
+int ReadData(int fd, uint16_t *datalen, uint8_t *data) {
+	int packagesize = 0;
+	int i = 0;
+	
+	//check for bytes to be read
+	packagesize = serialDataAvail(fd);
+	
+	if (packagesize > 0) {
+		while (i<packagesize) {
+			data[i] = serialGetchar(fd);
+			i++;
+		}
+	}
+	
+	*datalen = i;
+	return 0;
+};
+
+int SendData(int fd, uint16_t size, uint8_t *data) {
+	int i = 0;
+	for(i=0; i<size; i++) {
+		serialPutchar(fd, *(data+i));
+	}
+	return 0;
+};
