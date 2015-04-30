89176caadc2d4c277f7fbebd0c695f256e5967a1
David R. Meijer <meijer420@hotmail.com>
2015-04-30 10:01:03 +0200

    Update USBFunctions.h

--- Dronard/USBFunctions.h
+++ Dronard/USBFunctions.h
@@ -1,19 +1,17 @@
-#ifndef _MAVLINKFUNCTIONS_H_
-	#define _MAVLINKFUNCTIONS_H_
+#ifndef _USBFUNCTIONS_H_
+	#define _USBFUNCTIONS_H_
+	
+	#define BAUDRATE 	57600
+	#define APMDEVICE 	"/dev/ttyUSB0"
 	
 	#include <stdlib.h>
 	#include <stdio.h>
 	#include <stdint.h>
 	#include <wiringSerial.h>
 	
-	#define BAUDRATE 	115200
-	#define APMDEVICE 	"/dev/ttyAMA0"		//"/dev/ttyACM0"
-	
 	int OpenAPM(void);
 	void CloseAPM(int fd);
 	int ReadData(int fd, uint16_t *datalen, uint8_t *data);
-	//int SendData(int fd, uint16_t size, uint8_t *data);
+	int SendData(int fd, uint16_t size, uint8_t *data);
 	
-#endif //_MAVLINKFUNCTIONS_H_
-
-int SendData(int fd, uint16_t size, uint8_t *data);
+#endif //_USBFUNCTIONS_H_
